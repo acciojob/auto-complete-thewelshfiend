d5eb1eed86dd35c18de0d7d00121df8e169ac843
Kiran TJ <kiran.thomas.jacob@gmail.com>
2024-10-07 17:06:02 +0530

    Commit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-
 import React from "react";
 import './../styles/App.css';
+import { useState, useEffect } from "react";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+    const [updatedData, setUpdatedData] = useState([]);
+    const [val, setVal] = useState("");
+
+    useEffect(() => {
+        setUpdatedData(fruits.filter((item) => (item.toLowerCase().includes(val.toLowerCase()))));
+    }, [val]);
+
+    return (
+        <div>
+            {/* Do not remove the main div */}
+            <h1>Search item</h1>
+            <input type="text" name="search" value={val} onChange={(e) => (setVal(e.target.value))} />
+            <ul>
+                {
+                    updatedData.map((item, index) => <li key={index}>{item}</li>)
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default App
